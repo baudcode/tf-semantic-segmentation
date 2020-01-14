6d871d9a572aed114edac12c80e3541bfb8cc733
Malte Koch <m.koch@convaron.com>
2020-01-14 18:39:34 +0100

    Creating tfrecord from directory

--- tf_semantic_segmentation/bin/tfrecord_writer.py
+++ tf_semantic_segmentation/bin/tfrecord_writer.py
@@ -1,4 +1,4 @@
-from ..datasets import datasets_by_name, get_dataset_by_name, get_cache_dir
+from ..datasets import datasets_by_name, get_dataset_by_name, get_cache_dir, DirectoryDataset
 from ..processing.dataset import ColorMode, resize_and_change_color
 from ..datasets.tfrecord import TFWriter
 from ..settings import logger
@@ -11,7 +11,8 @@ import os
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dataset', choices=list(datasets_by_name.keys()), required=True)
+    parser.add_argument('-d', '--dataset', default=None, choices=list(datasets_by_name.keys()))
+    parser.add_argument('-dir', '--directory', default=None)
     parser.add_argument('-c', '--data_dir', required=True)
     parser.add_argument('-num', '--num_examples_per_record', default=100, type=int)
     parser.add_argument('-r', '--record_dir', default=None)
@@ -22,14 +23,24 @@ def main():
     parser.add_argument('-o', '--overwrite', action='store_true')
     args = parser.parse_args()
 
-    cache_dir = get_cache_dir(args.data_dir, args.dataset.lower())
-    ds = get_dataset_by_name(args.dataset, cache_dir)
+    if args.directory is None and args.dataset is None:
+        raise AssertionError("please either supply a dataset or a directory containing your data")
 
-    # write records
-    if args.record_dir:
-        record_dir = args.record_dir
+    if args.dataset:
+        cache_dir = get_cache_dir(args.data_dir, args.dataset.lower())
+        ds = get_dataset_by_name(args.dataset, cache_dir)
+
+        # write records
+        if args.record_dir:
+            record_dir = args.record_dir
+        else:
+            record_dir = os.path.join(cache_dir, 'records', args.dataset.lower())
     else:
-        record_dir = os.path.join(cache_dir, 'records', args.dataset.lower())
+        ds = DirectoryDataset(args.directory)
+        if args.record_dir is None:
+            raise AssertionError("record_dir arg cannot be None")
+
+        record_dir = args.record_dir
 
     def preprocess_fn(image, labels):
         image = tf.image.convert_image_dtype(image, tf.float32)

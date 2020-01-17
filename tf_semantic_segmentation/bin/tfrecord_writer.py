458de59c3e845739730321fe0102ad716919c322
Malte Koch <m.koch@convaron.com>
2020-01-17 21:28:14 +0100

    Made data_dir argument optional when writing tfrecord from directory

--- tf_semantic_segmentation/bin/tfrecord_writer.py
+++ tf_semantic_segmentation/bin/tfrecord_writer.py
@@ -12,10 +12,11 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', default=None, choices=list(datasets_by_name.keys()))
-    parser.add_argument('-dir', '--directory', default=None)
-    parser.add_argument('-c', '--data_dir', required=True)
-    parser.add_argument('-num', '--num_examples_per_record', default=100, type=int)
     parser.add_argument('-r', '--record_dir', default=None)
+
+    parser.add_argument('-dir', '--directory', default=None)
+    parser.add_argument('-c', '--data_dir', default=None)
+    parser.add_argument('-num', '--num_examples_per_record', default=100, type=int)
     parser.add_argument('-s', '--size', default=None, type=lambda x: list(map(int, x.split(','))))
     parser.add_argument('-rm', '--resize_method', default='resize_with_pad')
     parser.add_argument('-cm', '--color_mode', default=ColorMode.NONE, type=int)
@@ -27,6 +28,7 @@ def main():
         raise AssertionError("please either supply a dataset or a directory containing your data")
 
     if args.dataset:
+        assert(args.data_dir is not None), "data_dir argument is required"
         cache_dir = get_cache_dir(args.data_dir, args.dataset.lower())
         ds = get_dataset_by_name(args.dataset, cache_dir)
 
@@ -37,6 +39,7 @@ def main():
             record_dir = os.path.join(cache_dir, 'records', args.dataset.lower())
     else:
         ds = DirectoryDataset(args.directory)
+
         if args.record_dir is None:
             raise AssertionError("record_dir arg cannot be None")
 

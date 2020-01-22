c9190011797356e96cdcbb5a7ce2f74e0bb0388b
Malte Koch <m.koch@convaron.com>
2020-01-23 00:05:08 +0100

    Move tensorflow_addons import into functions

--- tf_semantic_segmentation/layers/__init__.py
+++ tf_semantic_segmentation/layers/__init__.py
@@ -3,15 +3,16 @@ from .minibatchstddev import MiniBatchStdDev
 from .pixel_norm import PixelNorm
 from .subpixel import Subpixel
 from .mixconv import MixConv
+from ..settings import logger
 
 import tensorflow as tf
-import tensorflow_addons as tfa
 
 
 def get_norm_by_name(name):
     if name == 'batch':
         return tf.keras.layers.BatchNormalization(axis=-1)
     elif name == 'instance':
+        import tensorflow_addons as tfa
         return tfa.layers.InstanceNormalization(axis=-1)
     elif name == 'pixel':
         return PixelNorm()

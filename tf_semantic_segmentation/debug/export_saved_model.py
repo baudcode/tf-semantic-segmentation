7814ea9b3a766f9e8705fab4957e14ae32fafaa8
Malte Koch <m.koch@convaron.com>
2020-01-26 21:21:57 +0100

    Move code to function export_saved_model

--- tf_semantic_segmentation/debug/export_saved_model.py
+++ tf_semantic_segmentation/debug/export_saved_model.py
@@ -4,13 +4,19 @@ from ..bin.train import main
 import argparse
 import os
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--logdir', required=True)
-    args = parser.parse_args()
 
-    model_path = os.path.join(args.logdir, 'model-best.h5')
+def export_saved_model(logdir, model_name='model-best.h5'):
+    model_path = os.path.join(logdir, model_name)
     model = load_model(model_path, compile=False)
 
     saved_model_path = os.path.join(args.logdir, 'saved_model', '0')
     model.save(saved_model_path, save_format='tf')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--logdir', required=True)
+    parser.add_argument('-m', '--model_name', default='model-best.h5')
+    args = parser.parse_args()
+
+    export_saved_model(args.logdir, args.model_name)

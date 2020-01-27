194aa76d9473b58bb7d40fc8cca75a803d7c593e
Malte Koch <m.koch@convaron.com>
2020-01-27 16:57:24 +0100

    Add get_model_description

--- tf_semantic_segmentation/models/__init__.py
+++ tf_semantic_segmentation/models/__init__.py
@@ -3,6 +3,7 @@ from .unet import unet, unet_v2
 from .multires_unet import multires_unet
 
 from tensorflow.keras.models import Model
+import inspect
 
 models_by_name = {
     "erfnet": erfnet,
@@ -10,6 +11,10 @@ models_by_name = {
     "unet_v2": unet_v2,
     "multires_unet": multires_unet
 }
+
+
+def get_model_description(name):
+    return inspect.getdoc(models_by_name[name])
 
 
 def get_model_by_name(name, args) -> (Model, Model):

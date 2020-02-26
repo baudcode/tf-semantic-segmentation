271e147698a1b6e41bd83d0506558fe5e84b6126
Malte Koch <m.koch@convaron.com>
2020-02-26 22:15:54 +0100

    Fixed imports

--- tf_semantic_segmentation/layers/__init__.py
+++ tf_semantic_segmentation/layers/__init__.py
@@ -1,4 +1,4 @@
-from .conv import fire, mixconv, grouped_conv_2d
+from .conv import Fire, MixConv, GroupedConv2D
 from .minibatchstddev import MiniBatchStdDev
 from .subpixel import Subpixel
 from .pixel_norm import PixelNorm
@@ -7,4 +7,4 @@ from .utils import get_norm_by_name
 import tensorflow as tf
 
 
-__all__ = ['fire', "MiniBatchStdDev", "PixelNorm", "mixconv", "grouped_conv_2d", "Subpixel", 'get_norm_by_name']
+__all__ = ['Fire', "MiniBatchStdDev", "PixelNorm", "MixConv", "GroupedConv2D", "Subpixel", 'get_norm_by_name']

128e389b33d771c0d1ed9cecc697888dff1b1636
baudcode <malte-koch@gmx.net>
2022-04-14 15:45:47 +0200

    Fit imports

--- tf_semantic_segmentation/models/deeplabv3plus.py
+++ tf_semantic_segmentation/models/deeplabv3plus.py
@@ -23,23 +23,16 @@ from typing import Tuple, Union
 
 import tensorflow as tf
 
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras import layers
-from tensorflow.python.keras.layers import Input
-from tensorflow.python.keras.layers import Lambda
-from tensorflow.python.keras.layers import Activation
-from tensorflow.python.keras.layers import Concatenate
-from tensorflow.python.keras.layers import Add
-from tensorflow.python.keras.layers import Dropout
-from tensorflow.python.keras.layers import BatchNormalization
-from tensorflow.python.keras.layers import Conv2D
-from tensorflow.python.keras.layers import DepthwiseConv2D
-from tensorflow.python.keras.layers import ZeroPadding2D
-from tensorflow.python.keras.layers import GlobalAveragePooling2D
+
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Lambda, Activation, \
+    Concatenate, Add, Dropout, BatchNormalization, Conv2D, \
+    Conv2D, DepthwiseConv2D, ZeroPadding2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+
+
 from tensorflow.python.keras.utils.layer_utils import get_source_inputs
-from tensorflow.python.keras.utils.data_utils import get_file
-from tensorflow.python.keras import backend as K
-from tensorflow.python.keras.applications.imagenet_utils import preprocess_input
+from tensorflow.keras import backend as K
 
 WEIGHTS_PATH_X = "https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.1/deeplabv3_xception_tf_dim_ordering_tf_kernels.h5"
 WEIGHTS_PATH_MOBILE = "https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.1/deeplabv3_mobilenetv2_tf_dim_ordering_tf_kernels.h5"

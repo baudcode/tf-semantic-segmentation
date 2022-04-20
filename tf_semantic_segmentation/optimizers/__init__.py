b31f2be957d41dc9d24543f0272a6bfce054f809
Malte Koch <malte-koch@gmx.net>
2022-04-20 20:36:51 +0200

    Add radam_warmup

--- tf_semantic_segmentation/optimizers/__init__.py
+++ tf_semantic_segmentation/optimizers/__init__.py
@@ -15,6 +15,15 @@ def get_optimizer_by_name(name, lr) -> optimizers.Optimizer:
         ranger = tfa.optimizers.Lookahead(radam, sync_period=6, slow_step_size=0.5)
         return ranger
 
+    elif name == 'radam_warmup':
+        import tensorflow_addons as tfa
+        return tfa.optimizers.RectifiedAdam(
+            lr=1e-3,
+            total_steps=10000,
+            warmup_proportion=0.1,
+            min_lr=lr,
+        )
+
     elif name == 'madgrad':
         from madgrad import MadGrad
         return MadGrad(lr=lr)

f2f203bedc14a06be9e316165bc1e14f66a309f5
Malte Koch <malte-koch@gmx.net>
2022-04-27 20:48:21 +0200

    Fix nested unet

--- tf_semantic_segmentation/models/nested_unet.py
+++ tf_semantic_segmentation/models/nested_unet.py
@@ -2,13 +2,17 @@ from tensorflow.keras import layers, regularizers
 from ..layers import get_norm_by_name
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
+import tensorflow as tf
 
 
-def conv(x, filters, kernel_size=(3, 3), l2=None, padding='SAME', activation='relu'):
+def conv(x, filters, kernel_size=(3, 3), l2=None, padding='SAME', activation='leaky_relu', dropout: float = None):
     y = layers.Conv2D(filters, kernel_size=kernel_size,
                       kernel_regularizer=regularizers.l2(l2) if l2 else None,
                       activation=None,
                       padding=padding)(x)
+    if dropout != None:
+        y = layers.Dropout(dropout)(y)
+
     y = get_norm_by_name('batch')(y)
     y = layers.Activation(activation)(y)
     return y
@@ -23,7 +27,7 @@ def nested_conv(x, filters, residual=False):
         return y
 
 
-def nested_unet(input_shape=(256, 256, 3), num_classes=2, num_first_filters=64, depth=4):
+def nested_unet(input_shape=(256, 256, 3), num_classes=2, num_first_filters=64, depth=5, deep_supervison: bool = True, merge_nested: bool = True):
     """UNet++ aims to improve segmentation accuracy, with a series of nested, dense skip pathways.
 
     Redesigned skip pathways made optimisation easier with the semantically similar feature maps.
@@ -31,7 +35,6 @@ def nested_unet(input_shape=(256, 256, 3), num_classes=2, num_first_filters=64, 
 
     https://arxiv.org/abs/1807.10165
     """
-    n1 = num_first_filters
     filters = [num_first_filters * pow(2, i) for i in range(5)]
     assert(depth in [3, 4, 5]), 'depth has to be either 3, 4 or 5'
 
@@ -69,18 +72,31 @@ def nested_unet(input_shape=(256, 256, 3), num_classes=2, num_first_filters=64, 
     x1_4 = nested_conv(K.concatenate([x1_0, x1_1, x1_2, x1_3, up(x2_3)]), filters[1])
     x0_5 = nested_conv(K.concatenate([x0_0, x0_1, x0_2, x0_3, x0_4, up(x1_4)]), filters[0])
 
-    if depth == 3:
-        output = layers.Conv2D(num_classes, kernel_size=(1, 1), activation=None)(x0_3)
-    elif depth == 4:
-        output = layers.Conv2D(num_classes, kernel_size=(1, 1), activation=None)(x0_4)
-    elif depth == 5:
-        output = layers.Conv2D(num_classes, kernel_size=(1, 1), activation=None)(x0_5)
-    else:
-        raise Exception("depth %d is invalid" % depth)
+    # outputs in reversed order
+    outputs = [
+        x0_1, x0_2, x0_3, x0_4, x0_5
+    ]
+    shapes = [x1_0.shape, x2_0.shape, x3_0.shape, x4_0.shape, x5_0.shape]
+    shapes = shapes[:depth][::-1]
+    outputs = outputs[:depth][::-1]
 
-    return Model(outputs=output, inputs=x)
+    if deep_supervison:
+        # apply upsamplingUpsampling2D
+        for i, (o, shape) in enumerate(zip(outputs, shapes)):
+            print(i, o.shape, o.name, shape)
+            outputs[i] = layers.Conv2D(num_classes, kernel_size=(1, 1), activation=None, name=f'prediction_{shape[1]}x{shape[2]}')(o)
+
+        if merge_nested:
+            merged = layers.Conv2D(num_classes, kernel_size=1, name="prediction", activation=None)(tf.concat(outputs, axis=-1))
+            outputs.append(merged)
+    else:
+        outputs = [layers.Conv2D(num_classes, kernel_size=(1, 1), activation=None, name='prediction')(outputs[0])]
+
+    return Model(outputs=outputs, inputs=x)
 
 
 if __name__ == "__main__":
-    model = nested_unet(depth=4)
-    model.summary()
+    model = nested_unet(depth=5)
+    for o in model.outputs:
+        print(o.name, o.shape)
+    # model.summary()
